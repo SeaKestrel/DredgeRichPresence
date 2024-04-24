901f39d2f2a5f7343052efe55441606e18f5275a
Jean Pantaléon <78849811+SeaKestrel@users.noreply.github.com>
2024-04-24 23:52:24 +0200

    Add files via upload

---
+++ DredgeRichPresence/Loader.cs
@@ -0,0 +1,21 @@
+﻿using DredgeRichPresence.Libs;
+using HarmonyLib;
+using UnityEngine;
+
+namespace DredgeRichPresence
+{
+	public class Loader
+	{
+		/// <summary>
+		/// This method is run by Winch to initialize your mod
+		/// </summary>
+		public static void Initialize()
+		{
+			var gameObject = new GameObject(nameof(DredgeRichPresence));
+			gameObject.AddComponent<DredgeRichPresence>();
+			gameObject.AddComponent<CustomSceneManager>();
+			GameObject.DontDestroyOnLoad(gameObject);
+            new Harmony(nameof(DredgeRichPresence)).PatchAll();
+        }
+	}
+}
